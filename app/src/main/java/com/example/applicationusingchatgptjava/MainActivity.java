34e7a1e4b8db9d4c54c91d19f4e95f4253a6f45e
kseongbin <kim940840@gmail.com>
2023-04-09 13:42:18 +0900

    commit ChatGPT app using Java

--- app/src/main/java/com/example/applicationusingchatgptjava/MainActivity.java
+++ app/src/main/java/com/example/applicationusingchatgptjava/MainActivity.java
@@ -2,13 +2,126 @@ package com.example.applicationusingchatgptjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView responseTV;
+    TextView questionTV;
+    TextInputEditText queryEdt;
+
+    String url = "https://api.openai.com/v1/completions";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        responseTV = findViewById(R.id.idTVResponse);
+        questionTV = findViewById(R.id.idTVQuestion);
+        queryEdt = findViewById(R.id.idEdtQuery);
+
+        queryEdt.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_SEND) {
+                    responseTV.setText("잠시만 기다려 주세요..");
+
+                    if (!queryEdt.getText().toString().isEmpty()) {
+                        getResponse(queryEdt.getText().toString());
+                    } else {
+                        Toast.makeText(MainActivity.this, "입력해 주세요....", Toast.LENGTH_SHORT).show();
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void getResponse(String query) {
+        questionTV.setText(query);
+        queryEdt.setText("");
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("model", "text-davinci-003");
+            jsonObject.put("prompt", query);
+            jsonObject.put("temperature", 0);
+            jsonObject.put("max_tokens", 100);
+            jsonObject.put("top_p", 1);
+            jsonObject.put("frequency_penalty", 0.0);
+            jsonObject.put("presence_penalty", 0.0);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        JsonObjectRequest postRequest = new JsonObjectRequest(Request.Method.POST, url, jsonObject, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    String responseMsg = response.getJSONArray("choices").getJSONObject(0).getString("text");
+                    responseTV.setText(responseMsg);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("TAGAPI", "Error is : " + error.getMessage() + "\n" + error);
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("Content-Type", "application/json");
+                params.put("Authorization", "Bearer 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");
+                return params;
+            }
+        };
+
+        postRequest.setRetryPolicy(new RetryPolicy() {
+            @Override
+            public int getCurrentTimeout() {
+                return 50000;
+            }
+
+            @Override
+            public int getCurrentRetryCount() {
+                return 50000;
+            }
+
+            @Override
+            public void retry(VolleyError error) throws VolleyError {
+            }
+        });
+
+        queue.add(postRequest);
+
     }
 }
